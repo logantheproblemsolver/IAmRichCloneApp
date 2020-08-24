5c61152f7698588bf0562385e147970e1a42b932
Logan Ellis <logan@onetouchbilling.com>
2020-08-24 10:13:26 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Logan Ellis on 8/24/20.
+//  Copyright © 2020 Logan Ellis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
